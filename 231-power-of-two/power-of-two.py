9df8b7d0d03e70407cbe44b839be33fe014bfc8f
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2025-09-06 18:07:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (81.11%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+     if n<=0:
+        return False
+     elif n>0:
+        if n&(n-1)==0:
+            return True
+        else:
+            return False
+     
