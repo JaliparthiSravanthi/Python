e5c69db37f8b11e58fc4f1fb686a7ec2d67f1bc8
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2025-10-27 23:06:23 +0530

    Time: 5 ms (50.16%) | Memory: 14.1 MB (30.59%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        x=nums[0]
+        for i in range(1,len(nums)):
+            x=x^nums[i]
+        return x
+           
+        
+        
+    
