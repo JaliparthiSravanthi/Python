4b509579daf203fc7cf45bf219b549872ffc487f
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-03-24 22:50:26 +0530

    Time: 1 ms (28.54%) | Memory: 46.3 MB (66.51%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -4,22 +4,14 @@ class Solution {
         int sum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<mat[i].length;j++){
-                if(n%2==0){
+                
                     if(i==j){
                         sum+=mat[i][j];
                     }
                     else if(i+j==n-1){
                         sum+=mat[i][j];
                     }
-                }
-                else{
-                    if(i==j){
-                        sum+=mat[i][j];
-                    }
-                    else if(i+j==n-1){
-                        sum+=mat[i][j];
-                    }
-                }
+
             }
         }
         return sum;
