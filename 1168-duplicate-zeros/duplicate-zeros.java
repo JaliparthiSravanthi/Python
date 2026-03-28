87ea4a8f9038464c0ff8d9189095acc275074336
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-03-28 16:09:57 +0530

    Time: 2 ms (40.50%) | Memory: 46.6 MB (93.49%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n=arr.length;
+       int[] a=new int[n];
+       int k=0;
+       for(int i=0;i<n && k<n;i++){
+        
+        if(arr[i]==0){
+            a[k]=0;
+            if(k<n){
+              a[k++]=0;
+            }
+        }
+        else{
+           a[k]=arr[i];}
+        k++;
+       } 
+     for(int i=0;i<n;i++){
+        arr[i]=a[i];
+     }
+     System.out.println(a);
+    }
+}
