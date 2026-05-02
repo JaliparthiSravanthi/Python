c9c96a4c319c8f8cec37ca5c5e4b604bec9cd166
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-02 14:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (32.18%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,8 +1,13 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if(matrix[i][j]==target):
-                    return True
+        r=0
+        c=len(matrix[0])-1
+        while(r<len(matrix) and c>=0):
+            if(matrix[r][c]==target):
+                 return True
+            elif(matrix[r][c]<target):
+                 r+=1
+            else:
+                c-=1
         return False
         
