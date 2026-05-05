18f5b4cfbd0882b65b43612530617f63952aeb78
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-05 11:53:35 +0530

    Time: 286 ms (97.07%) | Memory: 12.5 MB (22.67%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,12 +1,17 @@
 class Solution(object):
     def longestPalindrome(self, s):
         n=len(s)
-        mlen=""
+        longest=""
+        def executionfromcenter(left,right):
+            while(left>=0 and right<n and s[left]==s[right]):
+                left-=1
+                right+=1
+            return s[left+1:right]
         for i in range(n):
-            for j in range(i,n):
-                substring=s[i:j+1]
-                if substring==substring[ ::-1]:
-                    if len(substring)>len(mlen):
-                        mlen=substring
-        return mlen
-                        
+            odd_palindrome= executionfromcenter(i,i)
+            even_palindrome=executionfromcenter(i,i+1)
+            if(len(odd_palindrome)>len(longest)):
+                longest=odd_palindrome
+            if(len(even_palindrome)>len(longest)):
+                longest=even_palindrome
+        return longest
