ea5994631552f6937c36ec4e9a6d7ef3e459e19d
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-06 07:14:41 +0530

    Time: 359 ms (62.62%) | Memory: 12.5 MB (55.70%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        n=len(s)
-        longest=""
-        def executionfromcenter(left,right):
-            while(left>=0 and right<n and s[left]==s[right]):
-                left-=1
-                right+=1
-            return s[left+1:right]
-        for i in range(n):
-            odd_palindrome= executionfromcenter(i,i)
-            even_palindrome=executionfromcenter(i,i+1)
-            if(len(odd_palindrome)>len(longest)):
-                longest=odd_palindrome
-            if(len(even_palindrome)>len(longest)):
-                longest=even_palindrome
-        return longest
+       longest=""
+       def circular(left,right):
+        while(left>=0 and right<len(s) and s[left]==s[right]):
+            left-=1
+            right+=1
+        return s[left+1:right]
+       for i in range(len(s)):
+        odd_palindrome=circular(i,i)
+        even_palindrome=circular(i,i+1)
+        if(len(odd_palindrome)>len(longest)):
+            longest=odd_palindrome
+        if(len(even_palindrome)>len(longest)):
+            longest=even_palindrome
+       return longest  
+
