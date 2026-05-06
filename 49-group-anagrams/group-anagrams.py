e3cbba72cad744439ab0faf3c8ac3dae23282208
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-06 06:37:50 +0530

    Time: 15 ms (96.82%) | Memory: 16.4 MB (38.16%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,11 +3,12 @@ class Solution(object):
       l=[]
       for i in strs:
         l.append("".join(sorted(i)))
-        d={}
-      for k in range(len(l)):
-        key=l[k]
-        if key in d:
-            d[key].append(strs[k])
+      d={}
+      for j in range(len(l)):
+        k=l[j]
+        if k in d:
+            d[l[j]].append(strs[j])
         else:
-            d[key]=[strs[k]]
+            d[l[j]]=[strs[j]]
       return list(d.values())
+
