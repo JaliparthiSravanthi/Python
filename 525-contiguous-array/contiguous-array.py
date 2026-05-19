6e86dcc75a4ea33b8a3454a6b4a2aa3810ee7462
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-19 08:05:58 +0530

    Time: 104 ms (80.60%) | Memory: 16.6 MB (44.98%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,24 +1,16 @@
 class Solution(object):
     def findMaxLength(self, nums):
-       d={0:-1}
-       sum=0
-       l=0
-       mlen=0
-       for i in range(len(nums)):
-        if nums[i]==0:
+     d={0:-1}
+     mlen=0
+     l=0
+     sum=0
+     for i in range(len(nums)):
+        if(nums[i]==0):
             sum+=-1
         else:
             sum+=1
         if sum in d:
-            l=i-d[sum]
+            mlen=max(mlen,i-d[sum])
         else:
             d[sum]=i
-        mlen=max(l,mlen)
-       return mlen
-        
-
-
-
-
-                
-
+     return mlen
