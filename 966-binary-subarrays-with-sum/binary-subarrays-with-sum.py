7a6c4937e5f44f82c8d2b3930029754eb8ef84ea
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-20 09:45:12 +0530

    Time: 37 ms (60.53%) | Memory: 15.1 MB (21.93%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def numSubarraysWithSum(self, nums, goal):
         sum=0
-        c=0
         d={0:1}
+        c=0
         for i in nums:
             sum+=i
             if sum-goal in d:
                 c+=d[sum-goal]
-            if sum in d:
-                d[sum]+=1
-            else:
-                d[sum]=1
+            d[sum]=d.get(sum,0)+1
         return c
