74a67fefcb9bd5647d85c0ea3cb9ef6fad934dd5
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-21 10:31:48 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (33.33%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -4,19 +4,19 @@ class Solution {
         int l=0;
         int r=image[i].length-1;
         while(l<=r){
-            int temp=image[i][l];
-            image[i][l]=image[i][r];
+            int temp=image[i][l]^1;
+            image[i][l]=image[i][r]^1;
             image[i][r]=temp;
             l++;
             r--;
         }
-        for(int j=0;j<image[i].length;j++){
+        /*for(int j=0;j<image[i].length;j++){
             if(image[i][j]==1)
                 image[i][j]=0;
             else
               image[i][j]=1;
-        }
-    }  
+        } */  
+     }  
     return image; 
     }
 }
