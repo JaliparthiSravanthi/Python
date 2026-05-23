0212e728eac6d6ededbc8ba69cffa29db574aa92
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-23 21:48:14 +0530

    Time: 109 ms (31.44%) | Memory: 142 MB (43.61%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -4,20 +4,27 @@ class NumMatrix {
         a=new int[matrix.length][matrix[0].length];
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
+               if(i==0 && j==0)
                 a[i][j]=matrix[i][j];
+                else if(i==0 && j!=0)
+                  a[i][j]=matrix[i][j]+a[i][j-1];
+                else if(j==0 && i!=0)
+                  a[i][j]=matrix[i][j]+a[i-1][j];
+                if(i!=0 && j!=0)
+                 a[i][j]=matrix[i][j]+a[i-1][j]+a[i][j-1]-a[i-1][j-1];
             }
         }
         
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        for(int i=row1;i<=row2;i++){
-            for(int j=col1;j<=col2;j++){
-             sum+=a[i][j];
-            }
-        }
-    return sum;
+        int total = a[row2][col2];
+
+        int top = (row1 > 0) ? a[row1 - 1][col2] : 0;
+        int left = (col1 > 0) ? a[row2][col1 - 1] : 0;
+        int overlap = (row1 > 0 && col1 > 0) ? a[row1 - 1][col1 - 1] : 0;
+
+        return total - top - left + overlap;
     }
 }
 
