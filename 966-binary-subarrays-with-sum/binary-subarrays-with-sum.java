7a51dfa09f54e9faebe5efce3c00ef2167f59f9d
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-24 19:55:35 +0530

    Time: 23 ms (33.24%) | Memory: 52.8 MB (13.80%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,28 +1,15 @@
 class Solution {
-
-    public int atMost(int[] nums, int goal) {
-        if(goal < 0) return 0;
-
-        int l = 0;
-        int sum = 0;
-        int c = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-
-            sum += nums[i];
-
-            while(sum > goal) {
-                sum -= nums[l];
-                l++;
-            }
-
-            c += i - l + 1;
-        }
-
-        return c;
-    }
-
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal - 1);
+    HashMap<Integer,Integer> freq=new HashMap<>();
+    freq.put(0,1);
+    int sum=0;
+    int c=0;
+    for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
+        if(freq.containsKey(sum-goal))
+          c+=freq.get(sum-goal);
+        freq.put(sum,freq.getOrDefault(sum,0)+1);
     }
-}
+    return c;
+    }
+}   
