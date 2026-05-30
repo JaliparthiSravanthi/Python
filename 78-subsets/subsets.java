705d2212d0bd85254f92b9589374a980701cd113
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-05-30 11:10:03 +0530

    Time: 1 ms (89.14%) | Memory: 44.2 MB (56.29%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,15 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        int a=nums.length;
-        int n=1<<a;
-        for(int i=0;i<n;i++){
-            List<Integer> sub =new ArrayList<>();
-            for(int j=0;j<a;j++){
-                if((i&(1<<j))!=0) sub.add(nums[j]);
-            }
-            ans.add(sub);
+       List<List<Integer>> ans=new ArrayList<>();
+       helper(0,nums,new ArrayList<>(),ans);
+       return ans;
+    }
+    public static void helper(int i,int[] a,List<Integer> ds,List<List<Integer>> ans){
+        if(i==a.length){
+            ans.add(new ArrayList<>(ds));
+            return;
         }
-    return ans;
+        ds.add(a[i]);
+        helper(i+1,a,ds,ans);
+        ds.remove(ds.size()-1);
+        helper(i+1,a,ds,ans); 
     }
 }
