80c37db60c3b5d82d53a3938af87cdfa8bf80beb
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-06-14 08:41:00 +0530

    Time: 10 ms (53.06%) | Memory: 47.2 MB (5.07%) - LeetSync

--- 838-design-linked-list/design-linked-list.java
+++ 838-design-linked-list/design-linked-list.java
@@ -3,12 +3,16 @@ class MyLinkedList {
         class Node{
         int data;
         Node next;
+        Node prev;
         Node(int data){
             this.data=data;
+            this.next=null;
+            this.prev=null;
         }
-        Node(int data,Node next){
+        Node(int data,Node next,Node prev){
             this.data=data;
             this.next=next;
+            this.prev=prev;
         }
         }
         Node head;
@@ -18,94 +22,115 @@ class MyLinkedList {
     
     
     public int get(int index) {
-        Node temp=head;
-        int c=0;
-        while(temp!=null){
-            if(c==index){
-                return temp.data;
-            }
-            c++;
-            temp=temp.next;
-        }
+       Node temp=head;
+       int c=0;
+       while(temp!=null){
+        if(c==index) return temp.data;
+        c++;
+        temp=temp.next;
+       }
     return -1;
-
     }
     
     public void addAtHead(int val) {
-        Node x=new Node(val);
-        x.next=head;
-        head=x;
+       if(head==null){
+       head=new Node(val);
+       return ;
+       }
+       Node newnode=new Node(val,head,null);
+       head.prev=newnode;
+       head=newnode;
     }
     
     public void addAtTail(int val) {
-    if(head == null){
-       head = new Node(val);
-       return;
-      }
-        Node temp=head;
-        while(temp.next!=null){
-            temp=temp.next;
-        }
-        Node x=new Node(val);
-        temp.next=x;
+    if(head==null){
+         head=new Node(val);
+        return;
+    }
+    else if(head.next==null){
+        Node newnode=new Node(val,null,head);
+        head.next=newnode;
+        return;
+    }
+    Node temp=head;
+    while(temp.next!=null){
+        temp=temp.next;
+    }
+    Node newnode=new Node(val,null,temp);
+    temp.next=newnode;
     }
     
     public void addAtIndex(int index, int val) {
         if(head==null){
             if(index==0){
-                 Node x=new Node(val);
-            x.next=head;
-            head=x;
-            }
+             head=new Node(val);}
             return;
         }
         if(index==0){
-            Node x=new Node(val);
-            x.next=head;
-            head=x;
-            return;
+         Node newnode=new Node(val,head,null);
+         head.prev=newnode;
+         head=newnode;  
+         return;
         }
         Node temp=head;
-        Node prev=null;
         int c=0;
         while(temp!=null){
-            if(c==index){
-               Node x=new Node(val);
-               x.next=temp;
-               prev.next=x;
-               return;
-            }
-            c++;
-            prev=temp;
-            temp=temp.next;
+        if(c==index){
+        Node back=temp.prev;
+        Node newnode=new Node(val,temp,back);
+        back.next=newnode;
+        temp.prev=newnode;
+        return;
+        }
+        c++;
+        temp=temp.next;
         }
         if(c==index){
-            Node x=new Node(val);
-            prev.next=x;
+            Node tail=head;
+            while(tail.next!=null){
+                tail=tail.next;
+            }
+            Node newnode=new Node(val,null,tail);
+            tail.next=newnode;
         }
-    }
+           
+        
+     }
     
     public void deleteAtIndex(int index) {
         if(head==null) return;
-        if(index==0){
-            head=head.next;
-            return;
-        }
         Node temp=head;
-        Node prev=null;
         int c=0;
         while(temp!=null){
-            if(c==index){
-             prev.next=temp.next;
-             return;
-            }
+            if(c==index) break;
             c++;
-            prev=temp;
             temp=temp.next;
         }
+        if(temp==null) return;
+        Node back=temp.prev;
+        Node front=temp.next;
+        if(front==null && back==null){
+            head=null;
+            return;
+        }
+        if(back==null){
+            head=head.next;
+            head.prev=null;
+            temp.next=null;
+            return;
+        }
+        else if(front==null){
+            back.next=front;
+            temp.prev=null;
+            return;
+        }
+        back.next=front;
+        front.prev=back;
+        temp.next=null;
+        temp.prev=null;
+
     }
 }
-
 /**
  * Your MyLinkedList object will be instantiated and called as such:
  * MyLinkedList obj = new MyLinkedList();
