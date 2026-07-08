990d8726ae88c2f1c0e9040cf2bbdc113db39752
Sravanthi Jaliparthi <jaliparthisravanthi553@gmail.com>
2026-07-08 07:18:41 +0530

    Time: 12 ms (80.47%) | Memory: 46.4 MB (64.24%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,26 +1,18 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int vowels=0;
-        int mvowels=0;
-        for(int i=0;i<k;i++){
-            char cha = s.charAt(i);
-            if(cha==('a')||cha==('e')||cha==('i')||cha==('o')||cha==('u')){
-                vowels++;
+        int i=0;
+         int c=0;
+        int mcnt=0;
+        for(int j=0;j<s.length();j++){
+            char ch=s.charAt(j);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') c++;
+            if(j-i+1==k){
+                mcnt=Math.max(mcnt,c);
+                char cha=s.charAt(i);
+                 if(cha=='a' || cha=='e' || cha=='i' || cha=='o' || cha=='u') c--;
+                i++;
             }
-           
         }
-        mvowels=vowels;
-        for(int j=k;j<s.length();j++){
-            char ch = s.charAt(j-k);
-            if(ch==('a')||ch==('e')||ch==('i')||ch==('o')||ch==('u')){
-                vowels--;
-            }
-            char c=s.charAt(j);
-             if(c==('a')||c==('e')||c==('i')||c==('o')||c==('u')){
-                vowels++;
-            }
-            mvowels=Math.max(vowels,mvowels);
-        }
-        return mvowels;
+    return mcnt;
     }
-}
+}   
